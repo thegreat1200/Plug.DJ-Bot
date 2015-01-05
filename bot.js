d0ae385117c7e853a084a219070cc185230f4b22
thegreat1200 <junk_mail000@icloud.com>
2015-01-05 06:38:11 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,9 @@ sa = false;
 if (time > 23) {
 sa = true;
 }
+if (API.USER_JOIN) {
+  API.sendChat("/me User has joined!");
+}
 }, 5000);
 var room = $("#room-name").find(".bar-value").text();
 console.log("["+room+"Bot] Is now Online!");
