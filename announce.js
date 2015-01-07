b87be6250c7082f200800eb9c52542140057e435
thegreat1200 <junk_mail000@icloud.com>
2015-01-07 07:32:58 -0700

    Update announce.js
    Hypixel Network version.

--- announce.js
+++ announce.js
@@ -1,1 +1,10 @@
 window.setInterval(function(){
+API.on(API.ADVANCE, function(data){
+  API.sendChat("You are listning to Hypixel Network!");
+  API.sendChat("Next Song: "+data.media.title+" By: "+data.media.author);
+  API.sendChat("Next DJ: @"+data.dj.username);
+});
+}, 5000);
+var room = $("#room-name").find(".bar-value").text();
+console.log("[HypixelBot] Announcer is online!");
+API.sendChat("/me [HypixelBot] Announcer is online!");
