aafb6085145edfd483c56485e0be428ea87e736b
thegreat1200 <junk_mail000@icloud.com>
2015-01-10 12:58:10 -0700

    Update eta.js

--- eta.js
+++ eta.js
@@ -1,1 +1,15 @@
-d
+window.setInterval(function(){
+API.on(API.CHAT, function(data){
+var id = data.uid;
+var msg = data.message;
+var role = API.getUser(id).role;
+/* method what i use to see emote,message etc. content*/
+if(msg.indexOf("!commands") == 0 && role >=0){
+API.moderateDeleteChat(data.cid);
+API.sendChat(data.un+" has sent the command!");
+}
+});
+}, 5000);
+var room = $("#room-name").find(".bar-value").text();
+console.log("[HypixelBot] ETA is online!");
+API.sendChat("/me [HypixelBot] ETA is online!");
