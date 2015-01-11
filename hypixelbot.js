4122c0d4376ae7cd49da9ba36be26aa4375fa0d4
thegreat1200 <junk_mail000@icloud.com>
2015-01-11 08:49:13 -0700

    Update hypixelbot.js

--- hypixelbot.js
+++ hypixelbot.js
@@ -16,16 +16,12 @@ API.sendChat("/me ["+data.un+"] [!playnext] "+data.un+" is playing next.");
 if (msg.indexOf("!promote") == 0 && role >=5) {
   if (msg.indexOf("!promote @")) {
     var user = msg.substring(msg.length + 2);
-    for (var i = 0; i < API.getUsers().length; i++) {
-     if (API.getUsers()[i].id === user) {
-      var ouser = API.getUsers()[i];
-     }
-    }
     API.sendChat("User "+user+" has been promoted!");
   } else {
     API.sendChat("["+data.un+"] You need to specify a user!");
   }
 }
+});
 /*
 if (msg && role >= 0) {
   if (data.un !== API.getUser().username) {
