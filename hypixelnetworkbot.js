b59c63e0f6d58998078a995c71db91f000fc8398
thegreat1200 <junk_mail000@icloud.com>
2015-01-17 07:59:22 -0700

    Create hypixelnetworkbot.js

---
+++ hypixelnetworkbot.js
@@ -0,0 +1,6 @@
+window.setInterval(function(){
+  
+}, 5000);
+var room = $("#room-name").find(".bar-value").text();
+console.log("[HypixelBot] is online! V. Beta");
+API.sendChat("/me [HypixelBot] is online! V. Beta");
