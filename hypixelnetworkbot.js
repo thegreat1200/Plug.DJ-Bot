afc80c8bbebf551117b0366f106cf0e73f345f38
thegreat1200 <junk_mail000@icloud.com>
2015-01-17 10:29:46 -0700

    Update hypixelnetworkbot.js

--- hypixelnetworkbot.js
+++ hypixelnetworkbot.js
@@ -1,6 +1,18 @@
 window.setInterval(function(){
+  function activate() {
+    API.on(API.WAIT_LIST_UPDATE, waitlist(data));
+    API.sendChat("[HypixelBot] is online! V. Beta");
+    API.setStatus(API.STATUS.AVAILABLE);
+  }
   
+  function deactivate() {
+    API.off(API.WAIT_LIST_UPDATE, waitlist());
+    API.sendChat("[HypixelBot] is offline!");
+    API.setStatus(API.STATUS.WORKING);
+  }
+  
+  function waitlist(data) {
+    API.sendChat("==== Wait List ====");
+    API.sendChat("Next: "+API.getWaitlist()[0].)
+  }
 }, 5000);
-var room = $("#room-name").find(".bar-value").text();
-console.log("[HypixelBot] is online! V. Beta");
-API.sendChat("/me [HypixelBot] is online! V. Beta");
